f8975b348c5a32e30fee85ba70f09145eede89ce
Renaud Jenny <renaud.jenny@gmail.com>
2023-04-14 05:39:18 +0200

    feat(Dependency): move the `CustomStringConvertible` of Status

--- Sources/SwiftSpeechRecognizer/SpeechRecognitionStatus.swift
+++ Sources/SwiftSpeechRecognizer/SpeechRecognitionStatus.swift
@@ -6,14 +6,3 @@ public enum SpeechRecognitionStatus: Equatable {
     case stopping
     case stopped
 }
-
-extension SpeechRecognitionStatus: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .notStarted: return "not started"
-        case .recording: return "recording"
-        case .stopping: return "stopping"
-        case .stopped: return "stopped"
-        }
-    }
-}
--- Sources/SwiftSpeechRecognizerDependency/SwiftSpeechRecognizerDependency.swift
+++ Sources/SwiftSpeechRecognizerDependency/SwiftSpeechRecognizerDependency.swift
@@ -208,4 +208,15 @@ extension SFSpeechRecognizerAuthorizationStatus: CustomStringConvertible {
         }
     }
 }
+
+extension SpeechRecognitionStatus: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .notStarted: return "not started"
+        case .recording: return "recording"
+        case .stopping: return "stopping"
+        case .stopped: return "stopped"
+        }
+    }
+}
 #endif
